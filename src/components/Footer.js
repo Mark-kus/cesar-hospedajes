6568ee4509265f1a2297b2158688900ced3e33fb
Marco <tignanellimarco@gmail.com>
2023-04-24 09:21:28 -0300

    Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer () {
+    return (
+        <footer>
+            <div>
+                &copy; CesarHospedajes
+            </div>
+            <div>
+                All rights reserved &reg;
+            </div>
+        </footer>
+    )
+}
