27e3ee1358ef683d7dc4afdb3bf12ccc9998f82a
Marco <tignanellimarco@gmail.com>
2023-04-24 09:26:05 -0300

    Layout component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,16 @@
 import 'styles/globals.css'
 import { Provider } from 'react-redux'
 import { wrapper } from 'redux/store';
+import Layout from 'components/Layout.';
 
 export default function App({ Component, ...rest }) {
   const { store, props } = wrapper.useWrappedStore(rest);
   const { pageProps } = props;
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </Provider>
   )
 }
