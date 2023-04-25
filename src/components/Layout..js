cfeb369fb44dfb6b5a11a56f4f80555eaadf31c7
Marco <tignanellimarco@gmail.com>
2023-04-25 09:03:25 -0300

    layout fix

--- src/components/Layout..js
+++ src/components/Layout..js
@@ -1,10 +1,11 @@
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default function Layout({ children }) {
     return (
         <>
             <Navbar />
-            <children />
+            {children}
             <Footer />
         </>
     )
