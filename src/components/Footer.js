0793902dd6b5e824f7cc42b5c251b8e7f6743fa4
Marco <tignanellimarco@gmail.com>
2023-04-25 11:17:57 -0300

    basic styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 export default function Footer () {
     return (
-        <footer>
-            <div>
-                &copy; CesarHospedajes
+        <footer className="flex justify-between bg-slate-50">
+            <div className="p-2 text-slate-950">
+                &copy;CesarHospedajes
             </div>
-            <div>
-                All rights reserved &reg;
+            <div className="p-2 text-slate-950">
+                All rights reserved&reg;
             </div>
         </footer>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,16 @@ import Link from "next/link"
 
 export default function Navbar() {
     return (
-        <nav>
-            <div>
-                <Link href='/'>Imagen</Link>
-                <Link href='/hospedajes'>Hospedajes</Link>
-                <Link href='/hospedajes/new'>New</Link>
-                <Link href='/hospedajes/1'>Id</Link>
+        <nav className="flex justify-between bg-slate-900">
+            <div className="flex justify-between">
+                <Link className="hover:bg-slate-800 p-2" href='/'>CH</Link>
+                <Link className="hover:bg-slate-800 p-2" href='/hospedajes'>Hospedajes</Link>
+                <Link className="hover:bg-slate-800 p-2" href='/hospedajes/new'>New</Link>
+                <Link className="hover:bg-slate-800 p-2" href='/hospedajes/1'>Id</Link>
             </div>
-            <div>
-                <Link href='/login'>Login</Link>
-                <Link href='/register'>Register</Link>
+            <div className="flex justify-between">
+                <Link className="hover:bg-slate-800 p-2" href='/login'>Login</Link>
+                <Link className="hover:bg-slate-800 p-2" href='/register'>Register</Link>
             </div>
         </nav>
     )
