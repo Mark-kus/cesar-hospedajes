12ca3a0329270f47dd01d917ceb3d266ac261d0c
Marco <tignanellimarco@gmail.com>
2023-04-25 10:05:52 -0300

    semantic UI discarded

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,14 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="es">
-      <Head > 
-        {/* Semantic UI, delete for tailwind */}
-        <link
-          async
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
